400cfa6f3acf5392fe575b265f06898b715a4710
AllxCucu <cucumarianalexandru@gmail.com>
2018-06-08 00:01:51 +0300

    search_improved

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,7 @@ class Article < ApplicationRecord
   validates :title,
        presence: true,
  length: { minimum: 5 }
-
+ def self.search(search)
+  where("title LIKE ? OR text LIKE ?","%#{search}%","%#{search}%") 
+end
 end
